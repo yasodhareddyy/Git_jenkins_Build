2cca67cd4e4860254fbc430a28e477f2c738442b
LT-YASODHAS-324\YasodhaS-3242 <yasodhabharath06@gmailcom>
2023-09-05 13:29:03 +0530

    added modified

--- new_demo.py
+++ new_demo.py
@@ -4,3 +4,5 @@ b=40
 print(a+b)
 print("add new 1st line")
 print(a*b)
+New-Item -Path 'c:\' -Name 'minikube' -ItemType Directory -Force
+Invoke-WebRequest -OutFile 'c:\minikube\minikube.exe' -Uri 'https://github.com/kubernetes/minikube/releases/latest/download/minikube-windows-amd64.exe' -UseBasicParsing
